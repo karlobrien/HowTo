604065ca0dcdae9903e1923e1c6a43488a5c5902
Karl O'Brien <karlobrien@gmail.com>
2018-11-05 21:09:33 +0000

    simple example

--- src/HowTo.Common/DeferByAsync.cs
+++ src/HowTo.Common/DeferByAsync.cs
@@ -6,19 +6,23 @@ using System.Threading.Tasks;
 
 namespace HowTo.Common
 {
+    /// <summary>
+    /// An example of how to use defer async
+    /// </summary>
     public class DeferByAsync
     {
-        public IObservable<long> Me {get;}
+        public IObservable<long> Obs {get;}
 
         public DeferByAsync()
         {
-            var d = Observable.Interval(TimeSpan.FromSeconds(3))
+            Obs = Observable.Interval(TimeSpan.FromSeconds(3))
                 .Select(item => Observable.DeferAsync(async token => {
-                    return await StarterAsync(token, item);
-                }));
+                    return await ExpensiveTask(token, item);
+                }))
+                .Switch();
         }
 
-        private async Task<IObservable<long>> StarterAsync(CancellationToken token, long item)
+        private async Task<IObservable<long>> ExpensiveTask(CancellationToken token, long item)
         {
             var t = await Task.Run(() => {
                         Task.Delay(3000, token);
