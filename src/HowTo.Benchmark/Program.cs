3e730ad9322e36cd4b8d167a0a6db4701b6e4623
Karl O'Brien <karlobrien@gmail.com>
2018-11-06 06:53:56 +0000

    add benchmark example

---
+++ src/HowTo.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HowTo.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
