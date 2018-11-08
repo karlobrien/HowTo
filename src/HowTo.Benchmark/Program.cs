6dd00425b3d253924e6384230f3a21c2ef61f5d2
Karl O'Brien <karlobrien@gmail.com>
2018-11-08 16:57:58 +0000

    simple test for events

--- src/HowTo.Benchmark/Program.cs
+++ src/HowTo.Benchmark/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HowTo.Common;
 
 namespace HowTo.Benchmark
 {
@@ -7,6 +8,18 @@ namespace HowTo.Benchmark
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            SampleForEvent se = new SampleForEvent();
+            se.OnEvent += new MyEventHandler(MaxReached);
+
+            se.AddToNumber(2);
+            se.AddToNumber(9);
+            se.AddToNumber(4);
+        }
+
+        private static void MaxReached(object obj, MyEventArgs e)
+        {
+            Console.WriteLine(e.ToString());
         }
     }
 }
--- test/HowTo.Common.Tests/EventTest.cs
+++ test/HowTo.Common.Tests/EventTest.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using Xunit;
+using HowTo.Common;
 
 namespace HowTo.Common.Tests
 {
     public class EventTest
     {
+        private bool _myEventRaised;
+
         [Fact]
         public void Validate_EventHasBeenRaised()
         {
+            _myEventRaised = false;
+            SampleForEvent se = new SampleForEvent();
+            se.OnEvent += new MyEventHandler(MaxReached);
 
+            se.AddToNumber(2);
+            Assert.False(_myEventRaised);
+            se.AddToNumber(9);
+            Assert.False(_myEventRaised);
+            se.AddToNumber(2);
+            Assert.True(_myEventRaised);
+        }
+
+        private void MaxReached(object obj, MyEventArgs e)
+        {
+            _myEventRaised = true;
         }
     }
 }
