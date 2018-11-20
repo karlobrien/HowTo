29d3289c22e02a2ee91b45b77b45d105c90407db
Karl O'Brien <karlobrien@gmail.com>
2018-11-20 19:42:29 +0000

    thread me up

--- src/HowTo.Benchmark/Program.cs
+++ src/HowTo.Benchmark/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using HowTo.Common;
 using HowTo.GarbageCollection;
+using System.Threading;
 
 namespace HowTo.Benchmark
 {
@@ -8,33 +9,49 @@ namespace HowTo.Benchmark
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // SampleSubject ss = new SampleSubject();
+            // ss.Source.OnNext(1);
+            // ss.Source.OnNext(1);
+            // ss.Source.OnNext(1);
+            // ss.Source.OnNext(1);
 
-            SampleSubject ss = new SampleSubject();
-            ss.Source.OnNext(1);
-            ss.Source.OnNext(1);
-            ss.Source.OnNext(1);
-            ss.Source.OnNext(1);
+            // var item = Console.ReadLine();
+            // if (item == "q")
+            //     {
+            //         ss.Source.Dispose();
+            //     }
 
-            var item = Console.ReadLine();
-            if (item == "q")
-                return;
+            PollingConsumer pc = new PollingConsumer();
+            CancellationToken ct = new CancellationToken();
+            var obs = pc.StartConsuming(ct, TimeSpan.FromSeconds(1));
+            var dis = obs.Subscribe(sub => {
+                Console.WriteLine(sub);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine(Thread.CurrentThread.Name); //Consumer
+                Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread); //False
+            }, () => {
+                Console.WriteLine("Complete");
+            });
+
+            var stop = Console.ReadLine();
+            if (stop == "q")
+                {
+                    dis.Dispose();
+                    Console.WriteLine(ct.IsCancellationRequested); //false as token is not cancelled on sidpose
+                }
 
 
 
+            // SampleForEvent se = new SampleForEvent();
+            // se.OnEvent += new MyEventHandler(MaxReached);
 
+            // se.AddToNumber(2);
+            // se.AddToNumber(9);
+            // se.AddToNumber(4);
 
-
-            SampleForEvent se = new SampleForEvent();
-            se.OnEvent += new MyEventHandler(MaxReached);
-
-            se.AddToNumber(2);
-            se.AddToNumber(9);
-            se.AddToNumber(4);
-
-            int[] items = {1,2,3,4,5};
-            UnderstandingValueType uvt = new UnderstandingValueType();
-            Console.WriteLine(items[0]);
+            // int[] items = {1,2,3,4,5};
+            // UnderstandingValueType uvt = new UnderstandingValueType();
+            // Console.WriteLine(items[0]);
         }
 
         private static void MaxReached(object obj, MyEventArgs e)
